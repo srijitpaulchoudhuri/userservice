2b4db71ff27ef9e34f0cae50842e58bd0b13eec6
Srijit Choudhuri <Srijit_Choudhuri@epam.com>
2021-07-23 11:37:30 +0530

    Implemented sleuth,zipkin and ELBK

--- userservice/src/main/java/com/epam/app/restcontroller/UserController.java
+++ userservice/src/main/java/com/epam/app/restcontroller/UserController.java
@@ -2,6 +2,8 @@ package com.epam.app.restcontroller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,27 +24,34 @@ import com.epam.app.service.UserService;
 @RequestMapping("api/users")
 public class UserController {
 	
+	private final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private UserService userService;
 
 	@PostMapping
 	public ResponseEntity<User> add(@RequestBody UserDTO userDto){
+		logger.info("Adding new user");
 		return new ResponseEntity<>(userService.register(userDto),HttpStatus.CREATED);
 	}
 	@GetMapping
 	public ResponseEntity<List<User>> getAll(){
+		logger.info("Getting all users");
 		return new ResponseEntity<>(userService.getAll(),HttpStatus.OK);
 	}
 	@GetMapping("/{id}")
 	public ResponseEntity<User> get(@PathVariable int id){
+		logger.info("Getting one user with id {}",id);
 		return new ResponseEntity<>(userService.get(id),HttpStatus.OK);
 	}
 	@PutMapping("/{id}")
 	public ResponseEntity<User> update(@RequestBody UserDTO userDto,@PathVariable int id){
+		logger.info("Updating one user with id {}",id);
 		return new ResponseEntity<>(userService.update(userDto, id),HttpStatus.OK);
 	}
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> delete(@PathVariable int id){
+		logger.info("Deleting one user with id {}",id);
 		userService.delete(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
